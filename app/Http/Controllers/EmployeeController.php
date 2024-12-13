a9fdf9bc045a3d3fecd88fa4c13a2a0c7dd7f1bb
yunyuntr <149550729+yunyuntr@users.noreply.github.com>
2024-12-13 22:35:24 +0700

    Add files via upload

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,219 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
+use App\Models\Employee;
+use App\Models\Position;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+
+
+    public function index()
+    {
+        $pageTitle = 'Employee List';
+
+        // ELOQUENT
+        $employees = Employee::all();
+
+        return view('employee.index', [
+            'pageTitle' => $pageTitle,
+            'employees' => $employees
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+
+    public function create()
+    {
+        $pageTitle = 'Create Employee';
+
+        // ELOQUENT
+        $positions = Position::all();
+
+        return view('employee.create', compact('pageTitle', 'positions'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+
+
+    public function store(Request $request)
+    {
+        $messages = [
+            'required' => ':Attribute harus diisi.',
+            'email' => 'Isi :attribute dengan format yang benar',
+            'numeric' => 'Isi :attribute dengan angka'
+        ];
+
+        $validator = Validator::make($request->all(), [
+            'firstName' => 'required',
+            'lastName' => 'required',
+            'email' => 'required|email',
+            'age' => 'required|numeric',
+        ], $messages);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        // Get File
+        $file = $request->file('cv');
+
+        if ($file != null) {
+            $originalFilename = $file->getClientOriginalName();
+            $encryptedFilename = $file->hashName();
+
+            // Store File
+            $file->store('public/files');
+        }
+
+        // ELOQUENT
+        $employee = new Employee;
+        $employee->firstname = $request->firstName;
+        $employee->lastname = $request->lastName;
+        $employee->email = $request->email;
+        $employee->age = $request->age;
+        $employee->position_id = $request->position;
+
+        if ($file != null) {
+            $employee->original_filename = $originalFilename;
+            $employee->encrypted_filename = $encryptedFilename;
+        }
+
+        $employee->save();
+
+        return redirect()->route('employees.index');
+    }
+
+
+
+    /**
+     * Display the specified resource.
+     */
+
+
+    public function show(string $id)
+    {
+        $pageTitle = 'Employee Detail';
+
+        // ELOQUENT
+        $employee = Employee::find($id);
+
+        return view('employee.show', compact('pageTitle', 'employee'));
+    }
+
+
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+
+    public function edit(string $id)
+    {
+        $pageTitle = 'Edit Employee';
+
+        // ELOQUENT
+        $positions = Position::all();
+        $employee = Employee::find($id);
+
+        return view('employee.edit', compact('pageTitle', 'positions', 'employee'));
+    }
+
+    public function update(Request $request, string $id)
+    {
+        $messages = [
+            'required' => ':Atribute harus diisi.',
+            'email' => 'Isi :atribute dengan format yang benar.',
+            'numeric' => 'Isi :atribute dengan angka.'
+        ];
+
+        $validator = Validator::make($request->all(), [
+            'firstName' => 'required',
+            'lastName' => 'required',
+            'email' => 'required|email',
+            'age' => 'required|numeric',
+        ], $messages);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        // ELOQUENT
+        $employee = Employee::find($id);
+        $employee->firstname = $request->firstName;
+        $employee->lastname = $request->lastName;
+        $employee->email = $request->email;
+        $employee->age = $request->age;
+        $employee->position_id = $request->position;
+
+        // Proses File CV Baru
+        $file = $request->file('cv');
+
+        if ($file) {
+            // Hapus file CV (jika ada)
+            if ($employee->encrypted_filename) {
+                $oldFile = 'public/files/' . $employee->encrypted_filename;
+                if (Storage::exists($oldFile)) {
+                    Storage::delete($oldFile);
+                }
+            }
+
+            // Simpan file CV baru
+            $originalFilename = $file->getClientOriginalName();
+            $encryptedFilename = $file->hashName();
+
+            $file->store('public/files');
+
+            $employee->original_filename = $originalFilename;
+            $employee->encrypted_filename = $encryptedFilename;
+        }
+
+        $employee->save();
+
+        return redirect()->route('employees.index');
+    }
+
+    public function destroy(string $id)
+    {
+        // Temukan employee berdasarkan ID
+        $employee = Employee::findOrFail($id);
+
+        // Hapus file CV (jika ada)
+        if ($employee->encrypted_filename) {
+            $filePath = 'public/files/' . $employee->encrypted_filename;
+            if (Storage::exists($filePath)) {
+                Storage::delete($filePath);
+            }
+        }
+
+        // Hapus data employee dari database
+        $employee->delete();
+
+        return redirect()->route('employees.index');
+    }
+
+    public function downloadFile($employeeId)
+    {
+        $employee = Employee::find($employeeId);
+        $encryptedFilename = 'public/files/' . $employee->encrypted_filename;
+        $downloadFilename = Str::lower($employee->firstname . '_' . $employee->lastname . '_cv.pdf');
+
+        if (Storage::exists($encryptedFilename)) {
+            return Storage::download($encryptedFilename, $downloadFilename);
+        }
+
+        return redirect()->route('employees.index')->with('error', 'File not found.');
+    }
+}
